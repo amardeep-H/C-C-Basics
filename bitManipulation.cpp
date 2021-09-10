19935b94b8e8b40dfb1dbff020e21fa3f8e40fe3
Amardeep Honmane <rajdeepsonar@gmail.com>
2021-09-10 15:19:57 +0530

    Add files via upload

---
+++ bitManipulation.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+int getBit(int n, int pos)
+{
+    return ((n & (1 << pos)) != 0);
+}
+int setBit(int n, int pos)
+{
+    return (n | (1 << pos));
+}
+int clrBit(int n, int pos)
+{
+    int mask = ~(1 << pos);
+    return (n & mask);
+}
+int updateBit(int n, int pos, int value)
+{
+    int mask = ~(1 << pos);
+    n = n & mask;
+    return (n | value << pos);
+}
+int main()
+{
+    // cout << getBit(5, 2) << endl;
+    //cout << setBit(5, 1);
+    //cout << clrBit(5, 2) << endl;
+    cout << updateBit(5, 1, 1);
+    return 0;
+}
