1c421a71256266196469e44a34fb15b4460d51cc
Amardeep Honmane <rajdeepsonar@gmail.com>
2021-09-16 20:48:03 +0530

    Add files via upload

---
+++ findUniqueNumber.cpp
@@ -0,0 +1,8 @@
+//find unique term in an array where each term comes twice except 1 term which we have to find
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    cout << 1 + 66;
+    return 0;
+}
