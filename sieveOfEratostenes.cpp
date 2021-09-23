a4e4cfb30e443ef3b22ef0045120fcd4441e639d
Amardeep Honmane <rajdeepsonar@gmail.com>
2021-09-24 00:12:58 +0530

    Add files via upload

---
+++ sieveOfEratostenes.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+void primeSieve(int n)
+{
+    int prime[n] = {0};
+    for (int i = 2; i <= n; i++)
+    {
+        if (prime[i] == 0)
+        {
+            for (int j = i * i; j <= n; j = j + i)
+            {
+                prime[j] = 1;
+            }
+        }
+    }
+    for (int i = 2; i <= n; i++)
+    {
+        if (prime[i] == 0)
+        {
+            cout << i << " ";
+        }
+    }
+}
+int main()
+{
+    int n;
+    cin >> n;
+    primeSieve(n);
+    return 0;
+}
